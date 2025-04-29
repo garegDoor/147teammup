b9dfcc808f4ab73ada241a84915a1d760ecb4dc2
graceHerman <151581400+graceHerman@users.noreply.github.com>
2025-04-28 23:16:33 -0700

    put in ladybugs, slugs, and snails

--- Grace's Rock Gen/js/my_world.js
+++ Grace's Rock Gen/js/my_world.js
@@ -13,10 +13,12 @@
     p3_drawSelectedTile
     p3_drawAfter
 */
-let img;
+let img, bugImg, slugImg;
 
 function p3_preload() {
-  img = loadImage("https://cdn.glitch.global/8b4022f8-7c0a-4cd6-a8ca-db668d98e37d/snail.png?v=1745892976515");
+  slugImg = loadImage("https://cdn.glitch.global/8b4022f8-7c0a-4cd6-a8ca-db668d98e37d/Untitled16_20250428215652.png?v=1745906989878");
+  bugImg = loadImage("https://cdn.glitch.global/8b4022f8-7c0a-4cd6-a8ca-db668d98e37d/Untitled17_20250428220714.png?v=1745906987097");
+  img = loadImage("https://cdn.glitch.global/8b4022f8-7c0a-4cd6-a8ca-db668d98e37d/Untitled15_20250428213240.png?v=1745901198907");
 }
 
 let imgX = 0;  
@@ -24,27 +26,16 @@ let imgY = 0;
 let imgSpeedX = 0.5; 
 let imgSpeedY = 0.5;
 
-let snails = [];
+let bugs = [];
 let snailTile = [];
 
-function spawnSnail() {
-  snails.push({
+function spawnBug() {
+  bugs.push({
     x: random(width),
     y: random(height),
     speedX: random(-2, 2),
     speedY: random(-2, 2),
     size: 64
-  });
-}
-
-let worldSnails = [];
-
-function spawnWorldSnail(i, j) {
-  worldSnails.push({
-    i: i + random(-0.5, 0.5), // random offset within the tile
-    j: j + random(-0.5, 0.5),
-    di: random([-0.01, 0.01]),
-    dj: random([-0.01, 0.01])
   });
 }
 
@@ -55,15 +46,15 @@ function p3_setup() {
   background(0, 0, 0);
   
   for (let i = 0; i < 20; i++) {
-    spawnSnail();
+    spawnBug();
   }
   
-  for (let n = 0; n < 10; n++) {
+  for (let n = 0; n < 20; n++) {
     snailTile.push({
-      i: random(0, 10),
-      j: random(0, 10),
-      di: random([-0.02, 0.02]),  // horizontal speed
-      dj: random([-0.01, 0.01])   // vertical speed
+      i: random(-50, 50), // wider horizontal range
+      j: random(-50, 50), // wider vertical range
+      di: random([-0.02, 0.02]),
+      dj: random([-0.01, 0.01])
     });
   }
 }
@@ -142,32 +133,24 @@ function p3_drawTile(i, j) {
       let x = -tw + col * (brickW + 4) + offsetX;
       let y = -th + row * (brickH + 4);
       
-      //fill(random(175, 125) + random(-10, 10), 85 + random(-10, 10), 80 + random(-10, 10));  
       fill(160 + random(-10, 10), 120 + random(-10, 10), 75 + random(-10, 10));
-      //fill(random(170, 200) + random(-10, 10), random(130, 170) + random(-10, 10), random(50, 80) + random(-5, 5));
       rect(x, y, brickW - 4, brickH - 4, 5);
       
     }
     
   }
-  
-  /*// without the gaps
-  // Drawing bricks in a 4x4 grid
-  for (let row = 0; row < rows; row++) {
-    for (let col = 0; col < cols; col++) {
-      // stagger the bricks to every other row 
-      let offsetX = (row % 2 === 1) ? 20 : 0;
-      let x = -tw + col * (brickW) + offsetX;
-      let y = -th + row * (brickH);
-      
-      //fill(random(175, 125) + random(-10, 10), 85 + random(-10, 10), 80 + random(-10, 10));  
-      fill(160 + random(-10, 10), 120 + random(-10, 10), 70 + random(-10, 10));
-      rect(x, y, brickW, brickH, 5);
-    }
-    
-  }*/
-  
+
   pop();
+
+  randomSeed(XXH.h32("snailspawn:" + [i, j], worldSeed)); // consistent per tile
+
+  if (random() < 0.05) { 
+    push();
+    translate(0, -20);
+    imageMode(CENTER);
+    image(img, 0, 0, 48, 48);
+    pop();
+  }
   
   for (let snail of snailTile) {
     // Check if the snail is on this tile
@@ -175,20 +158,14 @@ function p3_drawTile(i, j) {
       push();
       translate(0, -20); // Raise snail above tile
       imageMode(CENTER);
-      image(img, 0, 0, 48, 48);
+      image(slugImg, 0, 0, 48, 48);
       pop();
     }
   }
   
   randomSeed(XXH.h32("slug:" + [i, j], worldSeed));
   let n = clicks[[i, j]] | 2;
-  if (n % 2 == 1) {
-    /*fill(0, 0, 0, 100);
-    ellipse(0, 0, 10, 5);
-    translate(0, -10);
-    fill(255, 255, 100, 255);
-    ellipse(0, 0, 10, 10);*/
-    
+  if (n % 2 == 1) {   
     // shadow
     translate(0, -10);
     fill(0, 0, 0, 85);
@@ -211,11 +188,10 @@ function p3_drawTile(i, j) {
     strokeWeight(1.5);
     line(12, -2, 16, -6);
     line(12, 2, 16, 6);
-    
-    
   }
 
 }
+
 
 function p3_drawSelectedTile(i, j) {
   noFill();
@@ -234,12 +210,12 @@ function p3_drawSelectedTile(i, j) {
 }
 
 function p3_drawAfter() {
-  for (let snail of snails) {
+  for (let bug of bugs) {
     // Draw the snail
-    image(img, snail.x, snail.y, snail.size, snail.size);
+    image(bugImg, bug.x, bug.y, bug.size, bug.size);
     
     // Move the snail
-    snail.x += snail.speedX;
-    snail.y += snail.speedY;
+    bug.x += bug.speedX;
+    bug.y += bug.speedY;
   }
 }
